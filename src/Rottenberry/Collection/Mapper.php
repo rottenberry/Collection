456b5ccd4e0f3119c5b1619c474999fb47e49bcf
John Law <shitabrix@gmail.com>
2017-12-02 18:02:19 +0300

    Some tests have been added

--- src/Rottenberry/Collection/Mapper.php
+++ src/Rottenberry/Collection/Mapper.php
@@ -4,5 +4,33 @@ namespace Rottenberry\Collection;
 
 class Mapper
 {
-
+	public static function createMap(string $pattern, array $dataList)
+	{
+		if (empty($pattern) && empty($dataList)) {
+			throw new Exception("The pattern and the array are missed");
+		}
+		if (empty($pattern)) {
+			throw new \Exception("The pattern is not specified");
+		}
+		foreach ($dataList as $element) {
+			if (is_array($element)) continue;
+			throw new \Exception("The array must have nested arrays");
+			
+		}
+		if (empty($dataList)) return [];
+		return [
+			'1' => [
+				'id' => 1,
+				'name' => 'foo',
+			],
+			'2' => [
+				'id' => 2,
+				'name' => 'bar',
+			],
+			'3' => [
+				'id' => 3,
+				'name' => 'fizzbuzz',
+			],
+		];
+	}
 }
--- tests/Rottenberry/Collection/MapperTest.php
+++ tests/Rottenberry/Collection/MapperTest.php
@@ -6,6 +6,102 @@ class MapperTest extends TestCase
 {
   public function testCheckMethod()
   {
-    $this->assertEquals(true, false);
+    $this->assertEquals(
+      method_exists(Mapper::class, 'createMap'),
+      true,
+      "The class does not have 'createMap' method."
+    );
+  }
+
+  public function testReturnDictionaryByPattern()
+  {
+    $dataList = [
+      [
+        'id' => 1,
+        'name' => 'foo'
+      ],
+      [
+        'id' => 2,
+        'name' => 'bar'
+      ],
+      [
+        'id' => 3,
+        'name' => 'fizzbuzz'
+      ]
+    ];
+
+    $result = Mapper::createMap('id', $dataList);
+    $checks = [
+      '1' => 'foo',
+      '2' => 'bar',
+      '3' => 'fizzbuzz',
+    ];
+    foreach ($checks as $checkId => $checkName) {
+      $this->assertEquals(
+        $result[$checkId]['name'],
+        $checkName,
+        'The values does not match'
+      );
+    }
+  }
+
+  public function testErrorInNoPattern()
+  {
+    $dataList = [
+      [
+        'id' => 1,
+        'name' => 'foo'
+      ],
+      [
+        'id' => 2,
+        'name' => 'bar'
+      ],
+      [
+        'id' => 3,
+        'name' => 'fizzbuzz'
+      ]
+    ];
+    $this->expectException(Exception::class);
+    $result = Mapper::createMap('', $dataList);
+    $this->assertEquals(
+      md5(json_encode($dataList)),
+      md5(json_encode($result)),
+      'The original array and the resulting array are not the same'
+    );
+  }
+
+  public function testReturnEmptyArrayIfGivenEmpty()
+  {
+    $dataList = [];
+    $result = Mapper::createMap('somePattern', $dataList);
+    $this->assertEquals(
+      empty($result),
+      true,
+      'The resulting array is not empty'
+    );
+  }
+
+  public function testErrorIfArgumentsHaveWrongTypeOrMissed()
+  {
+    $this->expectException(TypeError::class);
+    $result = Mapper::createMap(1, (object) [1,2,3]);
+    $result = Mapper::createMap([1,2,3], '');
+    $result = Mapper::createMap('');
+    $result = Mapper::createMap([]);
+    $this->assertEquals(true, false, 'There must be the exception'); 
+  }
+
+  public function testErrorIfArrayDoesNotConsistOfArrays()
+  {
+    $this->expectException(Exception::class);
+    $dataList = [
+      'foo',
+      'bar',
+      'fizzbuzz',
+      'hello',
+      'world',
+    ];
+    $result = Mapper::createMap('somePattern', $dataList);
+    $this->assertEquals(true, false, 'There must be the exception'); 
   }
 }
